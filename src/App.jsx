880f57fb9a7588c6ef4bb195eda3f8e0185b2bc5
ceridwenroberts <116894679+ceridwenroberts@users.noreply.github.com>
2023-02-19 00:16:55 +0100

    datepicker and countdown fn working

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,48 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
-import {formatDistanceToNow, isAfter, isBefore, parse, format, isToday, set} from 'date-fns'
-
+import { formatDistanceToNow, isAfter, isBefore, parse, format, isToday, set, parseISO, parseJSON, formatDistance, intervalToDuration, startOfDay } from 'date-fns'
+import useInterval from 'use-interval';
 import "react-datepicker/dist/react-datepicker.css";
 
-// CSS Modules, react-datepicker-cssmodules.css
-// import 'react-datepicker/dist/react-datepicker-cssmodules.css';
-
 function UserBirthday() {
-  const [startDate, setStartDate] = useState(new Date());
-  console.log("startDate before:" + startDate);
+const [startDate, setStartDate] = useState(new Date())
+const [untilBDay, setUntilBDay] = useState(getInterval)
+const [count, setCount] = useState(0)
+
+function getInterval () {
+  const interval = intervalToDuration( {
+    start: new Date(),
+    end: startOfDay(startDate)
+  })
+  return interval
+}
+
+useInterval(() => {
+  const interval = getInterval()
+  setUntilBDay(interval)
+  setCount(count + 1);
+}, 1000);
+
   return (
     <div>
-      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)}
+      <DatePicker selected={startDate}  onChange={(date) => setStartDate(date)}
         inline
-      // locale="sv-SE"
       />
-      {console.log("startDate:" + startDate)}
-      {console.log("getDate: " + startDate.getDate())}
-      {/* {console.log(startDate.toString())} */}
-      {console.log("getMinutes: " + startDate.getMinutes())}
-      {console.log("getDate: " + startDate.getDate())}
-      {console.log("toJSON: " + startDate.toJSON())}
-      {console.log(startDate.toJSON())}
-      {console.log(format(new Date(startDate), 'yyyy-MM-dd'))} 
-
-
-      <p>The selected date is: {startDate.toString()}</p>
+      <h1 className="days">{untilBDay.months} months<br/>
+            {untilBDay.days} days<br />
+            {untilBDay.hours}:{untilBDay.minutes}:{untilBDay.seconds}</h1>
+          <h2 className="time">count: {count}</h2>
+      <p>The selected date is: {startDate.toString()}.</p>
     </div>
   );
 }
 
 
-
-
-
 function App() {
+  // console.log(birthday);
   return (
     <div>
+      
       <UserBirthday />
     </div>
   )
